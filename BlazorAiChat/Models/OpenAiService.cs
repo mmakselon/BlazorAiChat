21e2850d116e7a924439baea9457ad3fdb239e4d
mmakselon <magdalena.pachacz@gmail.com>
2025-01-21 22:10:01 +0100

    add Models

---
+++ BlazorAiChat/Models/OpenAiService.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorAiChat.Models
+{
+    public class OpenAiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public OpenAiService(HttpClient httpClient, string apiKey)
+        {
+            _httpClient = httpClient;
+            _apiKey = apiKey;
+        }
+    }
+}
